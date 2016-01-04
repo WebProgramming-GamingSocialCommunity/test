8d431af366f0d293b3a67ab8e8e99cb7d0818911
potsrevennil <b02901144@ntu.edu.tw>
2016-01-04 11:35:41 +0800

    adjust routes of index.html and webpack.config and implement servet/routes/api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,10 +8,10 @@ var DB_PATH = __dirname + '/../db.json';
 
 /* GET users listing. */
 router.get('/posts', function(req, res, next) {
-  fs.readFile(DB_PATH, 'utf8', function(err, text) {
+  fs.readFile(DB_PATH, 'utf8', function(err, postContent) {
     if (err) return next(err);
     try {
-      var posts = JSON.parse(text);
+      var posts = JSON.parse(postContent);
       res.json(posts);
     } catch (e) {
       next(err);
@@ -22,7 +22,7 @@ router.get('/posts', function(req, res, next) {
 router.post('/posts', function(req, res, next) {
   console.log('req.body', req.body);
   var postContent = req.body.postContent;
-  console.log('text', text);
+  console.log('postContent', postContent);
   fs.readFile(DB_PATH, 'utf8', function(err, data) {
     if (err) return next(err);
     var posts;
